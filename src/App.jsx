763352c596e749acf80a5723a1817916d2f7ae82
Chavow5 <davidramirezw6@gmail.com>
2025-05-20 19:20:25 -0300

    header upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header'
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
 
 function App() {
   return (
@@ -7,8 +7,9 @@ function App() {
       <Header />
       <div>
         {/* componentes */}
+        <h1>hola</h1>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
